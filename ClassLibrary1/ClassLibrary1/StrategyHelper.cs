b2c9d89fc59d29ae1fcd000fd22dec94a1f08e5e
AlexExel <alex.mon.ra@gmail.com>
2014-04-24 12:04:08 +0300

    wealth lab version

---
+++ ClassLibrary1/ClassLibrary1/StrategyHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WealthLab;
+
+namespace ClassLibrary1
+{
+    class MyStrategyHelper : StrategyHelper
+    {
+        public override string Author
+        {
+            get { return "ASHab"; }
+        }
+
+        public override DateTime CreationDate
+        {
+            get { return new DateTime(2014, 03, 26, 12, 12,12); }
+        }
+
+        public override string Description
+        {
+            get { return "Example"; }
+        }
+
+        public override Guid ID
+        {
+            get { return new Guid("5F224023-4AE5-4248-8730-95C8E66FD27D"); }
+        }
+
+        public override DateTime LastModifiedDate
+        {
+            get { return DateTime.Now; }
+        }
+
+        public override string Name
+        {
+            get { return "MyStrategy"; }
+        }
+
+        public override Type WealthScriptType
+        {
+            get { return typeof(MyStrategyScript); }
+        }
+    }
+}
