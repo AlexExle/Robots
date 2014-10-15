8b1f114965be3524b50f34c23f334c4fb31bd756
Алибабаевич <alex.mon.ra@gmail.com>
2014-10-15 12:48:39 +0400

    bindings

---
+++ WL/bindings/ClassLibrary1/Binders/EnterBindings/Trend.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WealthLab;
+using WealthLab.Indicators;
+
+namespace ClassLibrary1.Binders.EnterBindings
+{
+    public class Trend : AEnterStrategy
+    {
+        public const int TrendPeriod = 100;
+        DataSeries trendMA;
+
+        public Trend(WealthScript strategyInstance)
+            : base(strategyInstance)
+        {
+            trendMA = SMA.Series(StrategyInstance.Bars.Close, TrendPeriod);
+            StrategyInstance.PlotSeries(StrategyInstance.PricePane, trendMA, Color.Red, LineStyle.Solid, 1);
+            firstValidValue = Math.Max(firstValidValue, trendMA.FirstValidValue);
+        }
+
+        public override EnterSignalType GenerateSignal(int bar)
+        {
+
+            if(StrategyInstance.Bars.Close[bar] > trendMA[bar]) // Закрытие бара выше медленной скользящей
+               return EnterSignalType.Up;
+           if (StrategyInstance.Bars.Close[bar] < trendMA[bar]) // Закрытие бара ниже медленной скользящей
+               return EnterSignalType.Down;
+           return EnterSignalType.None;
+        }
+    }
+}
---
+++ WL/bindings/ClassLibrary1/Binders/ExitBindings/StopAndReverse.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WealthLab;
+
+namespace ClassLibrary1.Binders.ExitBindings
+{
+    public class StopAndReverse : AExitStrategy
+    {
+        public StopAndReverse(WealthScript strategyInstance, bool isTrend)
+            : base(strategyInstance, isTrend)
+        { }
+
+        public override void InitExitConditions(WealthLab.Position position, int bar)
+        {
+
+        }
+
+        public override void RecalculateExitConditions(WealthLab.Position position, int bar)
+        {
+
+        }
+
+        public override bool TryExit(WealthLab.Position position, int bar, EnterSignalType lastEnterSignal)
+        {
+            if (lastEnterSignal == EnterSignalType.None)
+                return false;
+            if ((position.PositionType == PositionType.Long && ((lastEnterSignal == EnterSignalType.Down) && isTrend || !(lastEnterSignal == EnterSignalType.Down) && !isTrend)) || // Если находимся в длинной позиции, и пришел сигнал на вход в короткую позицию
+                (position.PositionType == PositionType.Short && ((lastEnterSignal == EnterSignalType.Up) && isTrend || !(lastEnterSignal == EnterSignalType.Up) && !isTrend))) // или находимся в короткой позиции, и пришел сигнал на вход в длинную позицию
+                return StrategyInstance.ExitAtMarket(bar + 1, position, "SAR"); // то выйти из позиции по рынку
+            return false;
+        }
+    }
+}
