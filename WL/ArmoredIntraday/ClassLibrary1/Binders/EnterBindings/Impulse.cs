b5544711f50a5dc8e036b43dfeb3afc1369167f9
Алибабаевич <alex.mon.ra@gmail.com>
2015-01-23 12:24:26 +0300

    initial commit

---
+++ WL/ArmoredIntraday/ClassLibrary1/Binders/EnterBindings/Impulse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WealthLab;
+
+namespace ClassLibrary1.Binders.EnterBindings
+{
+    public class Impulse : AEnterStrategy
+    {
+        public Impulse(WealthScript strategyInstance)
+            : base(strategyInstance)
+        {
+            firstValidValue = Math.Max(firstValidValue, 1); ;
+        }
+
+        public override EnterSignalType GenerateSignal(int bar)
+        {
+            if (StrategyInstance.Bars.Close[bar] > StrategyInstance.Bars.High[bar - 1])
+                return EnterSignalType.Up;
+            if (StrategyInstance.Bars.Close[bar] < StrategyInstance.Bars.Low[bar - 1])
+                return EnterSignalType.Down;
+            return EnterSignalType.None;
+        }
+    }
+}
---
+++ WL/ArmoredIntraday/ClassLibrary1/Binders/EnterBindings/TrandDoubleEmaCrossover.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WealthLab;
+using WealthLab.Indicators;
+
+namespace ClassLibrary1.Binders.EnterBindings
+{
+    class TrandDoubleEmaCrossover : AEnterStrategy
+    {
+        public const int TrendPeriod = 27;
+        public const int FastPeriod = 9;
+        DataSeries trendEma;
+        DataSeries fastEma;
+
+        public TrandDoubleEmaCrossover(WealthScript strategyInstance)
+            : base(strategyInstance)
+        {
+            trendEma = EMA.Series(StrategyInstance.Bars.Close, TrendPeriod, EMACalculation.Legacy);
+            StrategyInstance.PlotSeries(StrategyInstance.PricePane, trendEma, Color.Red, LineStyle.Solid, 1);
+            firstValidValue = Math.Max(firstValidValue, trendEma.FirstValidValue);
+
+            fastEma = EMA.Series(StrategyInstance.Bars.Close, FastPeriod, EMACalculation.Legacy);
+            StrategyInstance.PlotSeries(StrategyInstance.PricePane, fastEma, Color.Green, LineStyle.Solid, 1);
+            firstValidValue = Math.Max(firstValidValue, fastEma.FirstValidValue);
+        }
+
+        public override EnterSignalType GenerateSignal(int bar)
+        {
+
+            if (fastEma[bar] > trendEma[bar] && fastEma[bar-1] <= trendEma[bar-1]) // Пересечение сверху вниз
+               return EnterSignalType.Up;
+            if (fastEma[bar] < trendEma[bar] && fastEma[bar - 1] >= trendEma[bar - 1]) // Пересечение снизу вверх
+               return EnterSignalType.Down;
+           return EnterSignalType.None;
+        }
+    }
+}
