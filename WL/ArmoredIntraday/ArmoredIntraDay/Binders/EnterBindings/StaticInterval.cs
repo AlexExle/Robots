2608c3c2b37e953d78073005f052ededefcc7499
Алибабаевич <alex.mon.ra@gmail.com>
2015-03-17 09:12:46 +0300

    new enter

---
+++ WL/ArmoredIntraday/ArmoredIntraDay/Binders/EnterBindings/StaticInterval.cs
@@ -0,0 +1,23 @@
+﻿using ArmorediIntraday.Binders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WealthLab;
+
+namespace ArmoredIntraDay.Binders.EnterBindings
+{
+    public class StaticInterval : AEnterStrategy
+    {
+        public StaticInterval(WealthScript strategy):base(strategy)
+        {
+            
+        }
+
+        public override EnterSignalType GenerateSignal(int bar)
+        {
+            
+        }
+    }
+}
